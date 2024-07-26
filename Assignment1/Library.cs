d74ed13dd3f11392008e80a5ca0a01e5816aa457
Deni Prasetyo <19102048@ittelkom-pwt.ac.id>
2024-07-26 17:56:17 +0700

    Add project files.

---
+++ Assignment1/Library.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Library
+    {
+        private List<Book> bookCollection;
+        public Library()
+        {
+            bookCollection = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            if (bookCollection.Exists(b => b.Title.Equals(book.Title, StringComparison.OrdinalIgnoreCase)))
+            {
+                Console.WriteLine($"Buku dengan judul {book.Title} sudah ada \n");
+            }
+            else
+            {
+                bookCollection.Add(book);
+                Console.WriteLine($"Buku {book.Title} berhasil ditambah \n");
+            }
+                
+        }
+
+        public void DisplayBooks()
+        {
+            Console.WriteLine("Daftar Buku Tersedia : ");
+            foreach (Book book in bookCollection)
+            {
+                Console.WriteLine("Judul : {0}", book.Title);
+                Console.WriteLine("Author : {0}", book.Author);
+                Console.WriteLine("Publication Year : {0} \n", book.PublicationYear);
+            }
+        }
+
+        public void RemoveBook(string title)
+        {
+            Book removeBook = bookCollection.Find(b => b.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            if(removeBook != null)
+            {
+                bookCollection.Remove(removeBook);
+                Console.WriteLine($"Buku {title} berhasil dihapus \n");
+            }
+            else
+            {
+                Console.WriteLine($"Buku dengan judul {title} tidak ditemukan \n");
+            }
+            
+        }
+    }
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Assignment1;
+
+Library library = new Library();
+library.AddBook(new Book("Pengantar Informatika", "Deni", 2020));
+library.AddBook(new Book("Cara Cepat Belajar C#", "Nizar", 2021));
+library.AddBook(new Book("Belajar HTML", "Pandi", 2022));
+library.AddBook(new Book("Belajar HTML", "Pandi", 2022));
+
+library.DisplayBooks();
+
+library.RemoveBook("Belajar HTML");
+
+library.RemoveBook("Belajar HTML");
+
+library.DisplayBooks();
+
